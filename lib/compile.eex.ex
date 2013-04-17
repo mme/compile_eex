e12aeca2c8c4b2a0b8b062279082278e6c399943
Markus Ecker <markus.ecker@gmail.com>
2013-04-17 02:22:36 +0200

    Always recompile

--- lib/compile.eex.ex
+++ lib/compile.eex.ex
@@ -7,99 +7,33 @@ defmodule Mix.Tasks.Compile.Eex do
   @moduledoc """
   A task to compile Eex source files.
 
-  When this task runs, it will first check the mod times of
-  all of the files to be compiled and if they haven't been
-  changed since the last compilation, it will not compile
-  them at all. If any one of them has changed, it compiles
-  everything.
-
-  For this reason, this task touches your `:compile_path`
-  directory and sets the modification time to the current
-  time and date at the end of each compilation. You can
-  force compilation regardless of mod times by passing
-  the `--force` option.
-
-  Note it is important to recompile all files because
-  often there are compilation time dependencies between
-  the files (macros and etc). However, in some cases it
-  is useful to compile just the changed files for quick
-  development cycles, for such, a developer can pass
-  the `--quick` otion.
-
-  ## Command line options
-
-  * `--force` - forces compilation regardless of module times;
-  * `--quick`, `-q` - only compile files that changed;
-
   ## Configuration
 
   * `:eexc_paths` - directories to find source files.
-    Defaults to `["views"]`, can be configured as:
+    Defaults to `["templates"]`, can be configured as:
 
         [eexc_paths: ["templates", "other"]]
 
-  * `:eexc_options` - compilation options that applies
-     to Elixir's compiler, they are: `:ignore_module_conflict`,
-     `:docs` and `:debug_info`. By default, uses the same
-     behaviour as Elixir
-
-   * `:eexc_exts` - extensions to compile whenever there
-     is a change:
+   * `:eexc_exts` - extensions to compile
 
          [eexc_exts: [:html]]
-
-   * `:eexc_watch_exts` - extensions to watch in order to trigger
-      a compilation:
-
-         [eexc_watch_exts: [:html]]
          
   """
-  def run(args) do
-    { opts, _ } = OptionParser.parse(args, aliases: [q: :quick],
-                    switches: [force: :boolean, quick: :boolean])
+  def run(_args) do
 
     project       = Mix.project
     compile_path  = project[:compile_path]
     compile_exts  = project[:eexc_exts] || [:html]
-    watch_exts    = project[:eexc_watch_exts] || [:html]
     eexc_paths    = project[:eexc_paths] || ["templates"]
 
     to_compile = Mix.Utils.extract_files(eexc_paths, compile_exts)
-    to_watch   = Mix.Project.config_files ++ Mix.Utils.extract_files(eexc_paths, watch_exts)
-    stale      = Mix.Utils.extract_stale(to_watch, [compile_path])
     
-    IO.inspect to_watch
-    
-    if opts[:force] or stale != [] do
-      Mix.Utils.preserving_mtime(compile_path, fn ->
-        File.mkdir_p! compile_path
-        compile_files opts[:quick], project, compile_path, to_compile, stale
-      end)
-    
-      :ok
-    else
-      :noop
-    end
-  end
-
-  defp compile_files(true, project, compile_path, to_compile, stale) do
-    IO.inspect to_compile
-    opts = project[:elixirc_options] || []
-    opts = Keyword.put(opts, :ignore_module_conflict, true)
-    Code.compiler_options(opts)
-    to_compile = lc f inlist to_compile, List.member?(stale, f), do: f
+    File.mkdir_p! compile_path
     compile_files to_compile, compile_path
-  end
-  
-  defp compile_files(false, project, compile_path, to_compile, _stale) do
-    Code.delete_path compile_path
-    opts = project[:elixirc_options] || []
-    Code.compiler_options(opts)
-    compile_files to_compile, compile_path
-    Code.prepend_path compile_path
   end
   
   defp compile_files(files, to) do
+    Code.delete_path to
     Enum.each files, fn(file) ->
       module = make_module(make_module_name(file), file)
       [{module_name, code}] = Code.compile_string(module, file)
@@ -107,6 +41,7 @@ defmodule Mix.Tasks.Compile.Eex do
       File.write! path, code
       Mix.shell.info "Compiled #{file}"
     end
+    Code.prepend_path to
   end
   
   defp make_module_name(file), do: 
